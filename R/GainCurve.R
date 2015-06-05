f370545ccf7125684cfdab6dafb7fca3c531266e
Nina Zumel <nzumel@win-vector.com>
2015-06-05 15:24:56 -0700

    Start adding more common plots

---
+++ R/GainCurve.R
@@ -0,0 +1,50 @@
+
+# define some helper and reporting functions
+# calulcate area under the curve of numeric vectors x,y
+# length(x)==length(y)
+# y>=0, 0<=x<=1 and x increasing
+areaCalc <- function(x,y) {
+  # append extra points to get rid of degenerate cases
+  x <- c(0,x,1)
+  y <- c(0,y,1)
+  n <- length(x)
+  sum(0.5*(y[-1]+y[-n])*(x[-1]-x[-n]))
+}
+
+
+gainCurve = function(frame, xvar, truthVar,title='Gain Curve') {
+  truthcol <- as.numeric(frame[[truthVar]])
+  predcol <- as.numeric(frame[[xvar]])
+  # data frame of pred and truth, sorted in order of the predictions
+  d = data.frame(predcol=predcol,truthcol=truthcol)
+  predord = order(d[['predcol']], decreasing=TRUE) # reorder, with highest first
+  wizard = order(d[['truthcol']], decreasing=TRUE)
+  npop = dim(d)[1]
+
+  # data frame the cumulative prediction/truth as a function
+  # of the fraction of the population we're considering, highest first
+  results = data.frame(pctpop= (1:npop)/npop,
+                       model = cumsum(d[predord,'truthcol'])/sum(d[['truthcol']]),
+                       wizard = cumsum(d[wizard, 'truthcol'])/sum(d[['truthcol']]))
+
+  # calculate the areas under each curve
+  # gini score is 2* (area - 0.5)
+  idealArea = areaCalc(results$pctpop,results$wizard) - 0.5
+  modelArea = areaCalc(results$pctpop,results$model) - 0.5
+  giniScore = modelArea/idealArea # actually, normalized gini score
+
+  # melt the frame into the tall form, for plotting
+  results = melt(results, id.vars="pctpop", measure.vars=c("model", "wizard"),
+                 variable.name="sort_criterion", value.name="pct_outcome")
+
+  gplot = ggplot(data=results, aes(x=pctpop, y=pct_outcome, color=sort_criterion)) +
+    geom_point() + geom_line() +
+    geom_abline(color="gray") +
+    ggtitle(paste("Gain curve,", title, '\n',
+                  'relative Gini score', format(giniScore,digits=2))) +
+    xlab("% items in score order") + ylab("% total category") +
+    scale_x_continuous(breaks=seq(0,1,0.1)) +
+    scale_y_continuous(breaks=seq(0,1,0.1)) +
+    scale_color_manual(values=c('model'='darkblue', 'wizard'='darkgreen'))
+  gplot
+}
---
+++ R/ROC.R
@@ -0,0 +1,25 @@
+
+deviance <- function(truth,pred,epsilon=1.e-6) {
+  pred = pmax(pred, epsilon)
+  pred = pmin(pred, 1-epsilon)
+  S = 0.0 # assumed log-likelihood of saturated model
+  -2*(sum(ifelse(truth,log(pred),log(1-pred)))-S)
+}
+
+plotROC <- function(frame, xvar, truthVar,title='ROC plot') {
+  outcol <- frame[[truthVar]]
+  predcol <- frame[[xvar]]
+  pred <- prediction(predcol,outcol)
+  perf <- performance(pred,'tpr','fpr')
+  auc <- as.numeric(performance(pred,'auc')@y.values)
+  pf <- data.frame(
+    FalsePositiveRate=perf@x.values[[1]],
+    TruePositiveRate=perf@y.values[[1]])
+  plot=ggplot() +
+    geom_ribbon(data=pf,aes(x=FalsePositiveRate,ymax=TruePositiveRate,ymin=0),
+                fill='blue',alpha=0.3) +
+    geom_point(data=pf,aes(x=FalsePositiveRate,y=TruePositiveRate)) +
+    geom_line(aes(x=c(0,1),y=c(0,1))) + coord_fixed() +
+    ggtitle(paste(title,'\nAUC:',format(auc,digits=2)))
+  plot
+}
