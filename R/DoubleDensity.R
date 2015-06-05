a92d0c7b85719ec3414b383ed8f79a43ecbc0f15
John Mount <jmount@win-vector.com>
2015-06-05 15:48:40 -0700

    Work on double density a bit

--- R/DoubleDensity.R
+++ R/DoubleDensity.R
@@ -1,6 +1,16 @@
 
 
 doubleDensity <- function(frame, xvar, truthVar,title='double density plot') {
-  ggplot(frame, aes_string(x=xvar, color=truthVar)) +
-    geom_density(adjust=0.5,trim=TRUE)
+  df <- data.frame(x=as.numeric(frame[[xvar]]),
+                   y=as.character(frame[[truthVar]]),
+                   stringsAsFactors=FALSE)
+  pf <- ddply(df,'y',function(sf) {
+    dens = density(sf[['x']],adjust=0.5,
+                   from=min(sf[['x']]),to=max(sf[['x']]))
+    data.frame(truth=sf$y[[1]],
+               pred=dens$x,density=dens$y,
+                stringsAsFactors=FALSE)
+  })
+  ggplot(pf, aes(x=pred,ymin=0,y=density,ymax=density,color=truth,fill=truth)) +
+    geom_line() + geom_ribbon(alpha=0.5)
 }
