df5397d139d4f64167a56afc7f6c48a1379a3b88
John Mount <jmount@win-vector.com>
2015-06-06 08:17:24 -0700

    Better keys and linetypes on gain curve

--- R/GainCurve.R
+++ R/GainCurve.R
@@ -36,19 +36,27 @@ GainCurvePlot = function(frame, xvar, truthVar,title='Gain Curve') {
   # melt the frame into the tall form, for plotting
   results = melt(results, id.vars="pctpop", measure.vars=c("model", "wizard"),
                  variable.name="sort_criterion", value.name="pct_outcome")
-
-  gplot = ggplot(data=results, aes(x=pctpop, y=pct_outcome, color=sort_criterion)) +
+  # rename levels of sort criterion
+  colorKey = c('model'='darkblue', 'wizard'='darkgreen')
+  names(colorKey) = c(paste('model: sort by',xvar),paste('wizzard: sort by',truthVar))
+  modelKey = names(colorKey)[[1]]
+  results[["sort_criterion"]] = names(colorKey)[results[["sort_criterion"]]]
+  # plot
+  gplot = ggplot(data=results, aes(x=pctpop, y=pct_outcome,
+                                   color=sort_criterion,
+                                   shape=sort_criterion,
+                                   linetype=sort_criterion)) +
     geom_point() + geom_line() +
     geom_abline(color="gray") +
-    geom_ribbon(data=results[results$sort_criterion=='model',,drop=FALSE],
+    geom_ribbon(data=results[results$sort_criterion==modelKey,,drop=FALSE],
                 aes(x=pctpop, ymin=pctpop,ymax=pct_outcome, color=sort_criterion),
                 alpha=0.2,color=NA) +
     ggtitle(paste("Gain curve,", title, '\n',
                   truthVar, '~', xvar, '\n',
                   'relative Gini score', format(giniScore,digits=2))) +
-    xlab("% items in score order") + ylab("% total category") +
+    xlab("% items in sort order") + ylab(paste("% total sum",truthVar)) +
     scale_x_continuous(breaks=seq(0,1,0.1)) +
     scale_y_continuous(breaks=seq(0,1,0.1)) +
-    scale_color_manual(values=c('model'='darkblue', 'wizard'='darkgreen'))
+    scale_color_manual(values=colorKey)
   gplot
 }
