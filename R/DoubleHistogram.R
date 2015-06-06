2cb3c59ed4790347ea3af4950be0b495ddee1206
John Mount <jmount@win-vector.com>
2015-06-05 18:24:13 -0700

    unbalance data for demo weaken some linetypes

--- R/DoubleHistogram.R
+++ R/DoubleHistogram.R
@@ -9,15 +9,18 @@ doubleHistogram <- function(frame, xvar, truthVar,breaks=40,title='double histog
     counts = hist(sf[['x']],breaks=breaksV,plot=FALSE)
     rf = data.frame(count=counts$counts,
                stringsAsFactors=FALSE)
-    rf[truthVar]=sf$y[[1]]
-    rf[[xvar]]=counts$mids
+    rf[[xvar]] = counts$mids
+    rf[[truthVar]] = sf$y[[1]]
     sm = loess(paste('count','~',xvar),rf)
     rf$smooth = pmax(0,predict(sm,rf,se=FALSE))
+    # crudely match areas
+    scale = sum(rf$count)/sum(rf$smooth)
+    rf$smooth = rf$smooth*scale
     rf
   })
   ggplot() +
     geom_bar(data=pf,mapping=aes_string(x=xvar,color=truthVar,fill=truthVar,y='count'),
              stat='identity',alpha=0.5,position='identity') +
-    geom_line(data=pf,mapping=aes_string(x=xvar,color=truthVar,y='smooth')) +
+    geom_line(data=pf,mapping=aes_string(x=xvar,color=truthVar,y='smooth'),linetype=2) +
     ggtitle(title)
 }
