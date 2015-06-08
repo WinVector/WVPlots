26452f9d3b24cfc6e2802213ff0a52a0ba83c4aa
John Mount <jmount@win-vector.com>
2015-06-07 18:13:26 -0700

    Better idea on labeling

--- R/sharedFunctions.R
+++ R/sharedFunctions.R
@@ -1,4 +1,8 @@
 
+padToK <- function(k) {
+  force(k)
+  function(x) {str_pad(x,k,pad='_') }
+}
 
 # assumes p1 and p2 have not set scale_y_*
 alignPlotYlabels <- function(p1,p2) {
@@ -7,8 +11,7 @@ alignPlotYlabels <- function(p1,p2) {
   info2 <- ggplot_build(p2)
   origlabs2 <- info2$panel$ranges[[1]]$y.labels
   lengthTarget <- max(nchar(c(origlabs1,origlabs2)))
-  list(p1=p1+scale_y_continuous(breaks=as.numeric(origlabs1),
-                                labels=str_pad(origlabs1,lengthTarget,pad='_')),
-       p2=p2+scale_y_continuous(breaks=as.numeric(origlabs2),
-                                labels=str_pad(origlabs2,lengthTarget,pad='_')))
+  padFn <- padToK(lengthTarget)
+  list(p1=p1+scale_y_continuous(label=padFn),
+       p2=p2+scale_y_continuous(label=padFn))
 }
