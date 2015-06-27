919ab8f1a18b2371d360e345de76fdf6bfb3882b
Nina Zumel <nzumel@win-vector.com>
2015-06-26 18:14:42 -0700

    Added a Discrete Distribution function (subtly different from ClevelandDotPlot)

---
+++ R/DiscreteDistribution.R
@@ -0,0 +1,27 @@
+is_integral = function(x) {
+  if(!is.numeric(x)) return(FALSE)
+  if(is.integer(x)) return(TRUE)
+
+  xint = as.integer(x)
+  return(sum(xint != x)==0)
+}
+
+
+#' @export
+DiscreteDistribution = function(frm, xvar, stem=TRUE, title='Discrete Distribution') {
+  checkArgs(frm,xvar,xvar)
+  if(!is_integral(frm[[xvar]])) {
+    stop(paste("Column", xvar, "must have integer values"))
+  }
+
+  frm$unit=1
+
+  if(stem) {
+    geom="pointrange"
+  } else {
+    geom="point"
+  }
+  ggplot(frm, aes_string(xvar, "unit")) +
+    stat_summary(fun.y=sum, fun.ymax=sum, fun.ymin=function(x){0}, geom=geom) +
+    ggtitle(title)
+}
