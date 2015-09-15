bd3b6a3491cdbed83e2453079af8c7dc68f0ed91
John Mount <jmount@win-vector.com>
2015-09-14 17:58:09 -0700

    add horizontal scatter box plot rebuild documentation

--- R/ScatterBoxPlot.R
+++ R/ScatterBoxPlot.R
@@ -4,14 +4,15 @@ isDiscrete = function(x) {
 }
 
 
-#' Plot a scatter plot.  xvar is the independent variable (input or model) and yvar is the dependent variable
+#' Plot a scatter plot.  xvar is the discrete variable (input or model) and yvar is the continuous variable
 #' @param frame data frame to get values from
 #' @param xvar name of the independent column in frame; assumed discrete.
 #' if frm[[xvar]] is an interger column, it will be converted to a factor. This means that
 #' additional layers that rely on continuous x scales (like geom_smooth) won't work
-#' @param yvar name of the dependent column in frame
-#' @param pt_alpha transparency of points in scatter plot
+#' @param yvar name of the continuous column in frame
 #' @param title plot title
+#' @param ... (doesn't take additional arguments, used to force later arguments by name)
+#' @param pt_alpha transparency of points in scatter plot
 #'
 #' @export
 ScatterBoxPlot = function(frm, xvar, yvar, title='', ...,
@@ -29,4 +30,23 @@ ScatterBoxPlot = function(frm, xvar, yvar, title='', ...,
     geom_point(alpha=pt_alpha, position=position_jitter(width=0.1)) +
     ggtitle(title)
 
+}
+
+
+#' Plot a scatter plot in horizontal mode.
+#' xvar is the continuous variable and yvar is the discrete variable (input or model) and
+#' @param frame data frame to get values from
+#' @param xvar name of the continuous column in frame
+#' @param yvar name of the independent column in frame; assumed discrete.
+#' if frm[[yvar]] is an interger column, it will be converted to a factor. This means that
+#' additional layers that rely on continuous x scales (like geom_smooth) won't work
+#' @param title plot title
+#' @param ... (doesn't take additional arguments, used to force later arguments by name)
+#' @param pt_alpha transparency of points in scatter plot
+#'
+#' @export
+ScatterBoxPlotH = function(frm, xvar, yvar, title='', ...,
+                          pt_alpha=0.3) {
+  ScatterBoxPlot(frm, yvar, xvar, title=title, ...,
+                            pt_alpha=pt_alpha) + coord_flip()
 }
