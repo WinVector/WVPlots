088f19c6e51211bf6b78a58c459c12e667567e75
John Mount <jmount@win-vector.com>
2016-04-20 08:04:41 -0700

    new conditional ScatterHistC

--- R/ScatterHist.R
+++ R/ScatterHist.R
@@ -19,10 +19,7 @@
 #' set.seed(34903490)
 #' x = rnorm(50)
 #' y = 0.5*x^2 + 2*x + rnorm(length(x))
-#' frm = data.frame(x=x,y=y,yC=y>=as.numeric(quantile(y,probs=0.8)))
-#' frm$absY <- abs(frm$y)
-#' frm$posY = frm$y > 0
-#' frm$costX = 1
+#' frm = data.frame(x=x,y=y)
 #' WVPlots::ScatterHist(frm, "x", "y", title="Example Fit")
 #'
 #' @export
---
+++ R/ScatterHistC.R
@@ -0,0 +1,122 @@
+
+
+#' Plot a conditional scatter plot with marginals.  xvar is the independent variable (input or model), and yvar is the dependent variable, and cvar is the condition code
+#'
+#' @param frame data frame to get values from
+#' @param xvar name of the independent (input or model) column in frame
+#' @param yvar name of the dependent (output or result to be modeled) column in frame
+#' @param cvar name of condition variable
+#' @param title title to place on plot
+#' @param ...  no unnamed argument, added to force named binding of later arguments.
+#' @param annot_size numeric scale annotation text (if present)
+#' @param minimal_labels logical drop some annotations
+#' @param adjust_x  numeric adjust x density plot
+#' @param adjust_y  numeric adjust y density plot
+#' @examples
+#'
+#' set.seed(34903490)
+#' frm = data.frame(x=rnorm(50),y=rnorm(50))
+#' frm$cat <- frm$x+frm$y>0
+#' WVPlots::ScatterHistC(frm, "x", "y", "cat", title="Example Conditional Distribution")
+#'
+#' @export
+ScatterHistC = function(frame, xvar, yvar, cvar, title, ...,
+                       annot_size=5,
+                       minimal_labels = TRUE,
+                       adjust_x = 1,
+                       adjust_y = 1) {
+  checkArgs(frame=frame,xvar=xvar,yvar=yvar,title=title,...)
+
+  # placeholder plot - prints nothing at all
+  empty =  ggplot2::ggplot() +
+    ggplot2::geom_point(ggplot2::aes(c(0,1), c(0,1)), colour = "white") +
+    ggplot2::theme(plot.background = ggplot2::element_blank(),
+                   panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
+                   axis.title.x = ggplot2::element_blank(),
+                   axis.title.y = ggplot2::element_blank(),
+                   axis.text.x = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_blank(),
+                   axis.ticks = ggplot2::element_blank(),
+                   plot.margin = grid::unit(c(1, 1, 0, 0), "lines"))
+
+  # scatterplot of x and y
+  plot_center = ggplot2::ggplot(frame, ggplot2::aes_string(x=xvar,y=yvar,color=cvar)) +
+    ggplot2::geom_point() +
+    ggplot2::theme(plot.margin = grid::unit(c(0, 0, 0, 0), "lines")) +
+    scale_color_brewer(palette="Dark2") +
+    scale_fill_brewer(palette="Dark2")
+
+  # get the data range, to help align plots
+  x = frame[[xvar]]
+  y = frame[[yvar]]
+  xlims =  c(min(x), max(x))
+  ylims =  c(min(y), max(y))
+
+  #  print(xlims)
+  # print(ggplot_build(plot_center)$panel$ranges[[1]]$x.range)
+
+  plot_center = plot_center + ggplot2::xlim(xlims) + theme(legend.position="none")
+
+  # print(ggplot_build(plot_center)$panel$ranges[[1]]$x.range)
+
+  # marginal density of x - plot on top
+  #
+  # 0,0,0,0 -- title squooshed down
+  # 1,0,0,0 -- title has space
+  # 0,1,0,0 -- right side is shorter
+  # 0,0,1,0 -- bottom gap bigger
+  # 0,0,0,1 -- left side is shorter
+  #
+  plot_top <- ggplot2::ggplot(frame,
+                              ggplot2::aes_string(x=xvar,color=cvar)) +
+    ggplot2::geom_line(stat='density',adjust=adjust_x) +
+    ggplot2::xlim(xlims)
+  if(minimal_labels) {
+    plot_top = plot_top +
+      ggplot2::theme(legend.position = "none",
+                     axis.title.x = ggplot2::element_blank(),
+                     axis.text.x = ggplot2::element_blank(),
+                     axis.ticks.x = ggplot2::element_blank(),
+                     plot.margin = grid::unit(c(1, 0, 0, 0), "lines"))
+  } else {
+    plot_top = plot_top +
+      ggplot2::theme(plot.margin = grid::unit(c(1, 0, 0, 0), "lines"))
+  }
+  plot_top <- plot_top + scale_color_brewer(palette="Dark2") +
+    scale_fill_brewer(palette="Dark2")
+
+
+  # marginal density of y - plot on the right
+  plot_right <- ggplot2::ggplot(frame,
+                                ggplot2::aes_string(x=yvar,color=cvar)) +
+    ggplot2::geom_line(stat='density', adjust=adjust_y) +
+    ggplot2::xlim(ylims) +
+    ggplot2::coord_flip()
+  if(minimal_labels) {
+    plot_right = plot_right +
+      ggplot2::theme(legend.position = "none",
+                     axis.title.y = ggplot2::element_blank(),
+                     axis.text.y = ggplot2::element_blank(),
+                     axis.ticks.y = ggplot2::element_blank(),
+                     plot.margin = grid::unit(c(0, 1, 0, 0), "lines"))
+  } else {
+    plot_right = plot_right +
+      ggplot2::theme(plot.margin = grid::unit(c(0, 1, 0, 0), "lines"))
+  }
+  plot_right <- plot_right + scale_color_brewer(palette="Dark2") +
+    scale_fill_brewer(palette="Dark2")
+
+  yPadFn <- designYLabelPadFunction(plot_center + ggplot2::ylim(ylims),plot_top)
+  plot_center <- plot_center + ggplot2::scale_y_continuous(limits=ylims,label=yPadFn)
+  plot_top <- plot_top + ggplot2::scale_y_continuous(label=yPadFn)
+
+  # arrange the plots together, with appropriate height and width
+  # for each row and column
+
+  gridExtra::grid.arrange(plot_top, empty, plot_center, plot_right,
+                          top=grid::textGrob(title),
+                          ncol = 2, nrow = 2, widths = c(4,1), heights = c(1, 4))
+}
