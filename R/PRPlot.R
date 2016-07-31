cbe7f37394d16ff07a2b59657b37bd11b86f6455
John Mount <jmount@win-vector.com>
2016-07-31 08:44:35 -0700

    More annotations on pr plot. Change left-point convention.

--- R/PRPlot.R
+++ R/PRPlot.R
@@ -1,4 +1,15 @@
 
+
+# define some helper and reporting functions
+# calculate fraction of area above prevalance floor that is under curve
+# length(x)>=2
+# length(x)==length(y)
+# y>=0, x increasing and min(x)==0,max(x)==1
+prArea <- function(x,y) {
+  n <- length(x)
+  sum(0.5*(y[-1]+y[-n])*(x[-1]-x[-n]))
+}
+
 #' Plot Precision-Recall plot.
 #'
 #' See http://www.nature.com/nmeth/journal/v13/n8/full/nmeth.3945.html
@@ -38,11 +49,32 @@ PRPlot <- function(frame, xvar, truthVar,title,...) {
 
   # ROCR marks the pt where recall=0 as precision=NaN
   # get rid of that, and make it 1
-  isnan = which(is.nan(pf$Precision) && pf$Recall==0)
+  badPosns <-is.nan(pf$Precision) | is.na(pf$Precision) |
+                      is.infinite(pf$Precision) |
+                      (pf$Precision<0) | (pf$Precision>1) |
+                      is.nan(pf$Recall) | is.na(pf$Recall) |
+                      is.infinite(pf$Recall) |
+                      (pf$Recall<=0) | (pf$Recall>1)
+  pf <- pf[!badPosns,]
+  # add in ideal points
+  pf <- rbind(pf,data.frame(
+    Recall=c(0,1),
+    Precision=c(prevalence,prevalence)
+  ))
+  pf <- pf[order(pf$Recall),]
+  f1 <- 2*pf$Recall*pf$Precision/(pf$Recall+pf$Precision)
+  bestX <- which.max(f1)
+  bestF1 <- f1[[bestX]]
+  pF1 <- pf[bestX,]
 
-  if(length(isnan) > 0) {
-    pf$Precision[isnan]=1
-  }
+  pra <- prArea(pf$Recall,pf$Precision)
+
+  # curves of constant F1 with precision as a function of recall.
+  isoFrame <- data.frame(Recall=seq(0.01,1,by=0.01))
+  isoFrame$Precision <- bestF1*isoFrame$Recall/(2*isoFrame$Recall-bestF1)
+  isoFrame <- isoFrame[(isoFrame$Precision<=1) & (isoFrame$Precision>0),]
+  f1check <- 2*isoFrame$Recall*isoFrame$Precision/(isoFrame$Recall+isoFrame$Precision)
+
 
   palletName = "Dark2"
   plot= ggplot2::ggplot() +
@@ -50,14 +82,24 @@ PRPlot <- function(frame, xvar, truthVar,title,...) {
                          ggplot2::aes(x=Recall,ymax=Precision,ymin=0),
                          alpha=0.3) +
     ggplot2::geom_point(data=pf,
-                        ggplot2::aes(x=Recall,y=Precision)) +
+                        ggplot2::aes(x=Recall,y=Precision),
+                        alpha=0.8) +
+    ggplot2::geom_point(data=pF1,
+                        ggplot2::aes(x=Recall,y=Precision),
+                        color='blue',size=2,shape=15) +
     ggplot2::geom_line(data=pf,
                        ggplot2::aes(x=Recall,y=Precision)) +
+    ggplot2::geom_line(data=isoFrame,
+                       ggplot2::aes(x=Recall,y=Precision),
+                       color='blue',alpha=0.5,linetype=2) +
     ggplot2::geom_hline(yintercept=prevalence, linetype=2) +
     ggplot2::coord_fixed() +
     ggplot2::scale_fill_brewer(palette=palletName) +
     ggplot2::scale_color_brewer(palette=palletName) +
-    ggplot2::ggtitle(paste(title,'\n',
+    ggplot2::ggtitle(paste0(title,'\n',
+                           'best f1 ',format(bestF1, digits=2, nsmall=2),
+                           ', area ',format(pra, digits=2,nsmall=2),
+                           '\n',
                            truthVar, '~', xvar))
   plot
 }
