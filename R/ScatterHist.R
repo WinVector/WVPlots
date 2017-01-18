b799155fd2ef84e8c30b5be14a303e06f724ae24
John Mount <jmount@win-vector.com>
2017-01-17 19:14:28 -0800

    more smoothers, fewer messages

--- R/ScatterHist.R
+++ R/ScatterHist.R
@@ -9,7 +9,7 @@ NULL
 #' @param yvar name of the dependent (output or result to be modeled) column in frame
 #' @param title title to place on plot
 #' @param ...  no unnamed argument, added to force named binding of later arguments.
-#' @param smoothmethod (optional) one of 'auto' (the default), 'lm', or 'identity'.  If smoothmethod is 'auto' or 'lm' a smoothing curve or line (respectively) is added and R-squared of the best linear fit of xvar to yvar is reported.  If smoothmethod is 'identity' then the y=x line is added and the R-squared of xvar to yvar (without the linear transform used in the other smoothmethod modes) is reported.
+#' @param smoothmethod (optional) one of 'loess' (the default), 'auto', 'gam', 'lm', or 'identity'.  If smoothmethod is 'auto' or 'lm' a smoothing curve or line (respectively) is added and R-squared of the best linear fit of xvar to yvar is reported.  If smoothmethod is 'identity' then the y=x line is added and the R-squared of xvar to yvar (without the linear transform used in the other smoothmethod modes) is reported.
 #' @param annot_size numeric scale annotation text (if present)
 #' @param minimal_labels logical drop some annotations
 #' @param binwidth_x  numeric binwidth for x histogram
@@ -26,7 +26,7 @@ NULL
 #'
 #' @export
 ScatterHist = function(frame, xvar, yvar,title, ...,
-                       smoothmethod="auto", # only works for 'auto', 'lm', and 'identity'
+                       smoothmethod="loess", # only works for 'auto', 'loess', 'gam', 'lm', and 'identity'
                        annot_size=5,
                        minimal_labels = TRUE,
                        binwidth_x = NULL,
@@ -34,7 +34,7 @@ ScatterHist = function(frame, xvar, yvar,title, ...,
                        adjust_x = 1,
                        adjust_y = 1) {
   checkArgs(frame=frame,xvar=xvar,yvar=yvar,title=title,...)
-  if(!(smoothmethod %in% c('auto','lm','identity'))) {
+  if(!(smoothmethod %in% c('auto','loess','gam','lm','identity'))) {
     stop("smoothed method must be one of 'auto','lm', or 'identity'")
   }
   ..density.. <- NULL # used as a symbol, declare not an unbound variable
@@ -61,7 +61,7 @@ ScatterHist = function(frame, xvar, yvar,title, ...,
                     sigr::render(sigr::wrapFTest(frame,xvar,yvar),format='ascii'))
   }
   gSmooth = NULL
-  if(smoothmethod=='auto') {
+  if(smoothmethod %in%  c('auto','loess','gam')) {
     gSmooth = ggplot2::geom_smooth(method=smoothmethod)
   } else if(smoothmethod=="lm") {
     tryCatch({
@@ -124,7 +124,7 @@ ScatterHist = function(frame, xvar, yvar,title, ...,
   #
   plot_top <- ggplot2::ggplot(frame, ggplot2::aes_string(x=xvar)) +
     ggplot2::geom_histogram(ggplot2::aes(y=..density..), fill="gray",
-                   color="white", binwidth=binwidth_x) +
+                   color="white", binwidth=binwidth_x, bins=30) +
     ggplot2::geom_line(stat='density',color="blue", adjust=adjust_x) +
     ggplot2::xlim(xlims)
   if(minimal_labels) {
@@ -143,7 +143,7 @@ ScatterHist = function(frame, xvar, yvar,title, ...,
   # marginal density of y - plot on the right
   plot_right <- ggplot2::ggplot(frame, ggplot2::aes_string(x=yvar)) +
     ggplot2::geom_histogram(ggplot2::aes(y=..density..), fill="gray",
-                   color="white", binwidth=binwidth_y) +
+                   color="white", binwidth=binwidth_y, bins=30) +
     ggplot2::geom_line(stat='density',color="blue", adjust=adjust_y) +
     ggplot2::xlim(ylims) +
     ggplot2::coord_flip()
--- R/ScatterHistC.R
+++ R/ScatterHistC.R
@@ -17,7 +17,8 @@
 #' set.seed(34903490)
 #' frm = data.frame(x=rnorm(50),y=rnorm(50))
 #' frm$cat <- frm$x+frm$y>0
-#' WVPlots::ScatterHistC(frm, "x", "y", "cat", title="Example Conditional Distribution")
+#' WVPlots::ScatterHistC(frm, "x", "y", "cat",
+#'                       title="Example Conditional Distribution")
 #'
 #' @export
 ScatterHistC = function(frame, xvar, yvar, cvar, title, ...,
