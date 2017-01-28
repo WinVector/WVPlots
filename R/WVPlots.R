7cc19c4e845e4a22d71bd8e1e00ba334521415b4
John Mount <jmount@win-vector.com>
2017-01-28 12:53:33 -0800

    generate package help

---
+++ R/WVPlots.R
@@ -0,0 +1,18 @@
+#' WVPlots: Common Plots for Analysis
+#'
+#' Example 'ggplot2' plots we have found useful, under a standardized
+#' calling interface.
+#'
+#' To learn more about replyr, please start with the vignette:
+#' \code{vignette('replyr','replyr')}
+#'
+#'
+#'For more information:
+#' \itemize{
+#'   \item \code{vignette(package='WVPlots')}
+#'   \item \code{RShowDoc('WVPlots_examples',package='WVPlots')}
+#'   \item Website: \url{https://github.com/WinVector/WVPlots} }
+#'
+#' @docType package
+#' @name WVPlots
+NULL
