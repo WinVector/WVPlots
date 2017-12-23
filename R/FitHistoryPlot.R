d161e4a05f3f8a8f3a2868a1932b6508c07e0ce0
John Mount <jmount@win-vector.com>
2017-12-23 09:31:18 -0800

    add direct Keras example

--- R/FitHistoryPlot.R
+++ R/FitHistoryPlot.R
@@ -81,8 +81,6 @@ plot_fit_trajectory <- function(d,
     controlTable = column_description,
     columnsToCopy = epoch_name)
 
-
-
   # get factors into user order
   d$measure <- factor(d$measure,
                        levels = column_description$measure)
@@ -117,7 +115,11 @@ plot_fit_trajectory <- function(d,
     ggplot2::ggtitle(title)
   if(!is.null(pick_metric)) {
     pd <- d[d$measure == pick_metric, , drop = FALSE]
-    pick <- pd[[epoch_name]][[which.max(pd$validation)]]
+    if(!is.null(discount_rate)) {
+      pick <- pd[[epoch_name]][[which.max(pd$discounted)]]
+    } else {
+      pick <- pd[[epoch_name]][[which.max(pd$validation)]]
+    }
     plt <- plt +
       ggplot2::geom_vline(xintercept = pick, alpha=0.7, color='#e6ab02')
   }
@@ -131,6 +133,61 @@ plot_fit_trajectory <- function(d,
                 method = "loess",
                 linetype = 2)
   }
-
   plt
+}
+
+
+#' Plot the trajectory of a Keras model fit.
+#'
+#' Plot a history of fit over a trajetory of times.  Example below gives
+#' a fit plot for a history report from Keras R package.
+#' Please see \url{http://winvector.github.io/FluidData/PlotExample/KerasPerfPlot.html}
+#' for some details.
+#'
+#' @param d data frame to get values from.
+#' @param title character title for plot.
+#' @param ... force later arguments to be bound by name
+#' @param epoch_name name for epoch or trajectory column.
+#' @param pick_metric character metric to maximize.
+#' @param discount_rate numeric what fraction of over-fit to subtract from validation performance.
+#' @return ggplot2 plot
+#'
+#' @examples
+#'
+#' # example data (from Keras)
+#' d <- data.frame(
+#'   epoch    = c(1,         2,         3,         4,         5),
+#'   val_loss = c(0.3769818, 0.2996994, 0.2963943, 0.2779052, 0.2842501),
+#'   val_acc  = c(0.8722000, 0.8895000, 0.8822000, 0.8899000, 0.8861000),
+#'   loss     = c(0.5067290, 0.3002033, 0.2165675, 0.1738829, 0.1410933),
+#'   acc      = c(0.7852000, 0.9040000, 0.9303333, 0.9428000, 0.9545333) )
+#'
+#' plt <- plot_Keras_fit_trajectory(
+#'   d,
+#'   title = "model performance by epoch, dataset, and measure")
+#'
+#' suppressWarnings(print(plt)) # too few points for loess
+#'
+#' @export
+#'
+plot_Keras_fit_trajectory <- function(d,
+                                title,
+                                ...,
+                                epoch_name = "epoch",
+                                pick_metric = "minus binary cross entropy",
+                                discount_rate = 0.1) {
+  d[[epoch_name]] <- seq_len(nrow(d))
+  column_description <-  data.frame(
+       measure =    c("minus binary cross entropy", "accuracy"),
+       training =   c("loss",                       "acc"),
+       validation = c("val_loss",                   "val_acc"),
+       stringsAsFactors = FALSE)
+  needs_flip <- "minus binary cross entropy"
+  plot_fit_trajectory(d = d,
+                      title = title,
+                      column_description = column_description,
+                      needs_flip = needs_flip,
+                      epoch_name = epoch_name,
+                      pick_metric = pick_metric,
+                      discount_rate = discount_rate)
 }
