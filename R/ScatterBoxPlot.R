8c2113468083147f5504c3f8d62a84197fff78d4
Nina Zumel <nzumel@win-vector.com>
2019-09-11 15:43:34 -0700

    Added color controls to ScatterBoxPlots. Also fixed the problem with integer groupings. No longer have to convert to factors.

--- R/ScatterBoxPlot.R
+++ R/ScatterBoxPlot.R
@@ -5,8 +5,6 @@
 #' Plot a boxplot with the data points superimposed.
 #'
 #' \code{xvar} is a discrete variable and \code{yvar} is a continuous variable.
-#' If \code{frm[[xvar]]} is an integer column, it will be converted to a factor.
-#' This means that additional layers that rely on continuous x scales (like geom_smooth) won't work.
 #'
 #' @param frm data frame to get values from
 #' @param xvar name of the independent column in frame; assumed discrete
@@ -14,6 +12,9 @@
 #' @param title plot title
 #' @param ... (doesn't take additional arguments, used to force later arguments by name)
 #' @param pt_alpha transparency of points in scatter plot
+#' @param pt_color point color
+#' @param box_color boxplot line color
+#' @param box_fill boxplot fill color (can be NA for no fill)
 #'
 #' @seealso \code{\link{ScatterBoxPlotH}}
 #'
@@ -30,7 +31,10 @@
 #'
 #' @export
 ScatterBoxPlot = function(frm, xvar, yvar, title, ...,
-                          pt_alpha=0.3) {
+                          pt_alpha=0.3,
+                          pt_color='black',
+                          box_color='black',
+                          box_fill='lightgray') {
   frm <- as.data.frame(frm)
   check_frame_args_list(...,
                         frame = frm,
@@ -40,13 +44,10 @@ ScatterBoxPlot = function(frm, xvar, yvar, title, ...,
   if(!isDiscrete(frm[[xvar]])) {
     stop(paste(xvar, "should be discrete (factor, character, integer, or logical)"))
   }
-  if(is.integer(frm[[xvar]])) {
-    frm[[xvar]] = as.factor(frm[[xvar]])
-  }
 
-  ggplot2::ggplot(frm, ggplot2::aes_string(x=xvar, y=yvar)) +
-    ggplot2::geom_boxplot(outlier.size=0, fill="lightgray") +
-    ggplot2::geom_point(alpha=pt_alpha, position=ggplot2::position_jitter(width=0.1,height=0)) +
+  ggplot2::ggplot(frm, ggplot2::aes_string(x=xvar, y=yvar, group=xvar)) +
+    ggplot2::geom_boxplot(outlier.size=0, color=box_color, fill=box_fill) +
+    ggplot2::geom_point(color=pt_color, alpha=pt_alpha, position=ggplot2::position_jitter(width=0.1,height=0)) +
     ggplot2::ggtitle(title)
 }
 
@@ -56,8 +57,6 @@ ScatterBoxPlot = function(frm, xvar, yvar, title, ...,
 #' Plot a boxplot with the data points superimposed. Box plots are aligned horizontally.
 #'
 #' \code{xvar} is a continuous variable and \code{yvar} is a discrete variable.
-#' If frm[[yvar]] is an integer column, it will be converted to a factor.
-#' This means that additional layers that rely on continuous y scales won't work.
 #'
 #'
 #' @param frm data frame to get values from
@@ -66,7 +65,9 @@ ScatterBoxPlot = function(frm, xvar, yvar, title, ...,
 #' @param title plot title
 #' @param ... (doesn't take additional arguments, used to force later arguments by name)
 #' @param pt_alpha transparency of points in scatter plot
-#'
+#' @param pt_color point color
+#' @param box_color boxplot line color
+#' @param box_fill boxplot fill color (can be NA for no fill)
 #' @seealso \code{\link{ScatterBoxPlot}}
 #'
 #' @examples
@@ -84,7 +85,10 @@ ScatterBoxPlot = function(frm, xvar, yvar, title, ...,
 #' @export
 ScatterBoxPlotH = function(frm, xvar, yvar, title,
                            ...,
-                           pt_alpha=0.3) {
+                           pt_alpha=0.3,
+                           pt_color='black',
+                           box_color='black',
+                           box_fill='lightgray') {
   frm <- as.data.frame(frm)
   check_frame_args_list(...,
                         frame = frm,
@@ -92,5 +96,7 @@ ScatterBoxPlotH = function(frm, xvar, yvar, title,
                         title = title,
                         funname = "WVPlots::ScatterBoxPlotH")
   ScatterBoxPlot(frm, yvar, xvar, title=title, ...,
-                 pt_alpha=pt_alpha) + ggplot2::coord_flip()
+                 pt_alpha=pt_alpha, pt_color=pt_color,
+                 box_color=box_color, box_fill=box_fill) +
+    ggplot2::coord_flip()
 }
