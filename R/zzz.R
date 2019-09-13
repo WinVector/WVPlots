c7ae36a038a522b07940ba65a5ea1ee4595adc77
John Mount <jmount@win-vector.com>
2019-09-13 14:09:23 -0700

    qualify rqdatatable fn name

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
 .onAttach <- function(libname, pkgname) {
   prev_exec <- getOption("rquery.rquery_executor", default = NULL)
   if(is.null(prev_exec) || (is.list(prev_exec) && isTRUE(prev_exec$name == "rqdatable"))) {
-    options(list("rquery.rquery_executor" = list(f = ex_data_table, name = "rqdatable")))
+    options(list("rquery.rquery_executor" = list(f = rqdatatable::ex_data_table, name = "rqdatable")))
   } else {
     packageStartupMessage("rqdatatable loaded, but did not register itself as the executor (already one registered)")
   }
@@ -15,7 +15,7 @@
 .onLoad <- function(libname, pkgname) {
   prev_exec <- getOption("rquery.rquery_executor", default = NULL)
   if(is.null(prev_exec) || (is.list(prev_exec) && isTRUE(prev_exec$name == "rqdatable"))) {
-    options(list("rquery.rquery_executor" = list(f = ex_data_table, name = "rqdatable")))
+    options(list("rquery.rquery_executor" = list(f = rqdatatable::ex_data_table, name = "rqdatable")))
   }
   invisible()
 }
